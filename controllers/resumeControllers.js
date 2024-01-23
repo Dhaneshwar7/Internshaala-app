c0eca8e9496c555504204b479bc6d4dbd51106fd
Delta Mike <87529812+DELTAMIKE7@users.noreply.github.com>
2024-01-23 13:58:52 +0530

    Resume model education Add Created

--- controllers/resumeControllers.js
+++ controllers/resumeControllers.js
@@ -1,7 +1,25 @@
 const { catchAsyncError } = require('../middlewares/catchAsyncError');
 const Student = require('../models/studentModel');
+const { v4: uuidv4 } = require('uuid');
 const ErrorHandler = require('../utils/ErrorHandlers');
 
-exports.resume = catchAsyncError((req, res, next) => {
-	res.json({ message: 'Resume of USER Internshala' });
+exports.resume = catchAsyncError(async (req, res, next) => {
+	const { resume } = await Student.findById(req.id).exec();
+
+	res.json({ message: 'Resume of USER Internshala', resume });
+});
+
+exports.addeducation = catchAsyncError(async (req, res, next) => {
+	const student = await Student.findById(req.id).exec();
+	student.resume.education.push({ ...req.body, id: uuidv4() });
+	await student.save();
+	res.json({ message: 'Education Added', resume: student.resume });
+});
+
+
+exports.editeducation = catchAsyncError(async (req, res, next) => {
+	const student = await Student.findById(req.id).exec();
+	student.resume.education.push({ ...req.body, id: uuidv4() });
+	await student.save();
+	res.json({ message: 'Education Added', resume: student.resume });
 });
--- routes/resumeRoutes.js
+++ routes/resumeRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { resume } = require('../controllers/resumeControllers');
+const { resume, addeducation } = require('../controllers/resumeControllers');
 const { isAuthenticated } = require('../middlewares/auth');
 
 const router = express.Router();
@@ -7,6 +7,10 @@ const router = express.Router();
 // GET /
 router.get('/', isAuthenticated, resume);
 
+//POST ADD EDUCATION
+router.post('/add-education', isAuthenticated, addeducation);
 
+//POST ADD EDUCATION
+router.post('/add-education', isAuthenticated, addeducation);
 
 module.exports = router;
