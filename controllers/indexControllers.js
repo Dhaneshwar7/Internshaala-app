6f6795f3144fc590ac7f1564e6f56de7a37f8959
Delta Mike <87529812+DELTAMIKE7@users.noreply.github.com>
2024-07-17 15:52:35 +0530

    message added

--- controllers/indexControllers.js
+++ controllers/indexControllers.js
@@ -9,8 +9,8 @@ const path = require('path');
 const imageKit = require('../utils/imageKit').uploadImagekit();
 
 exports.homepage = catchAsyncError((req, res, next) => {
-	res.send(`<div style="width: 100vw;height: 100vh; display: flex; align-items: center; justify-content: center;">
-            <h1 class="ok"  style="font-size: 10vw; font-weight: 800; text-align: center;">Internshala Server is Running ! Thank you 🙏</h1>
+	res.send(` <div style="width: 100vw;height: 100vh; display: flex; align-items: center; justify-content: center;">
+            <h1 class="ok"  style="font-size: 10vw; font-weight: 800; text-align: center;padding: 5vw; color: green;">Internshala Server is Running <br> ! Thank you 🙏</h1>
         </div>`);
 });
 
